aa35ac41fd72e9709a1f59ed571fefb0ca6120b2
Anton Kuznetsov <anton66656@yandex.ru>
2019-04-12 07:02:44 +0300

    Добавляет условие2

--- index.php
+++ index.php
@@ -25,28 +25,24 @@ $user_name = 'Anton'; // укажите здесь ваше имя
             <input class="main-header__search-btn" type="submit" name="find" value="Найти">
         </form>
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
-
         <nav class="user-menu">
-
-        <?php 
-        if ($is_auth == 1) {
-<div class="user-menu__logged">
-    <p>$user_name</p>
-    <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-    <a class="user-menu__logout" href="#">Выход</a>
-  </div>
-        }
-    else {
-        <ul class="user-menu__list">
-        <li class="user-menu__item">
-          <a href="#">Регистрация</a>
-        </li>
-        <li class="user-menu__item">
-          <a href="#">Вход</a>
-        </li>
-      </ul>
-    }
-?>
+ 
+        <?php if ($is_auth == 1): ?>
+            <div class="user-menu__logged">
+                <p> <?php print ($user_name); ?></p>
+                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                <a class="user-menu__logout" href="#">Выход</a>
+            </div>
+        
+        <?php else :?> 
+            <ul class="user-menu__list">
+            <li class="user-menu__item">
+              <a href="#">Регистрация</a>
+            </li>
+            <li class="user-menu__item">
+              <a href="#">Вход</a>
+            </li>
+          </ul> 
         </nav>
     </div>
 </header>
