c9343870d3559f03dcaa2077841d633e81f1e387
Anton Kuznetsov <anton66656@yandex.ru>
2019-04-14 18:45:26 +0300

    Пишем первую функцию

--- index.php
+++ index.php
@@ -133,7 +133,7 @@ function sum_of_good($price) {
                                 <div class="lot__state">
                                     <div class="lot__rate">
                                         <span class="lot__amount">Стартовая цена</span>
-                                        <span class="lot__cost"><?=sum_of_good($val['price']);?><b class="rub">&ndash; &#8381;</b></span>
+                                        <span class="lot__cost"><?=sum_of_good($val['price']);?><b class="rub"> &ndash; &#8381;</b></span>
                                     </div>
                                     <div class="lot__timer timer">
                                         12:23
