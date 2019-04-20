3e3a0f97732811200080ec82ceb21a18a05ace76
Anton Kuznetsov <anton66656@yandex.ru>
2019-04-20 09:58:52 +0300

    Шаблонизация проекта v.3

--- index.php
+++ index.php
@@ -42,34 +42,7 @@ $goods = [
     ]
 ];
 
-function sum_of_goods($price) {
-        $output = 0;
-        if ($price > 1000) {
-            $output = ceil($price);
-            $output = number_format($price, 0, ',', ' ');
-        }
-        else {
-            $output = ceil($price);
-        }
-      return $output;
-}
-
-function include_template($name, array $data = []) {
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!is_readable($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
-}
+include_once 'functions.php'; 
 
 $page_content = include_template('index.php',['categories'=>$categories, 'goods'=> $goods]);
 $layout_content = include_template('layout.php',[
