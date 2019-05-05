a931f002869bae194cfeb4232d078b949f15b314
Anton Kuznetsov <anton66656@yandex.ru>
2019-05-05 14:00:22 +0300

    Чтение и показ записей из MySQL v.2

--- index.php
+++ index.php
@@ -4,14 +4,20 @@ mysqli_set_charset($con, "utf8");
 
 $sql = "SELECT name_category from categories";
 
-$res = mysqli_query($con, $sql); 
-$rows = mysqli_fetch_all($res, MYSQLI_ASSOC);
+if ($res = mysqli_query($con, $sql)) {
+    $rows = mysqli_fetch_all($res, MYSQLI_ASSOC);
+} else {
+    $error = mysqli_connect_error();
+    die('Unknown error');
+}
 
 $sql = "SELECT lot_name, picture, first_price, name_category from lots l
         JOIN categories c ON l.category_id = c.id";
-
-$res = mysqli_query($con, $sql); 
-$goods = mysqli_fetch_all($res, MYSQLI_ASSOC);
+if ($res = mysqli_query($con, $sql)) {
+    $goods = mysqli_fetch_all($res, MYSQLI_ASSOC);
+} else {
+    die('Unknown error');
+}
 
 $is_auth = rand(0, 1);
 $user_name = 'Anton'; // укажите здесь ваше имя
